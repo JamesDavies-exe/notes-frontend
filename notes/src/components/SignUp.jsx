a9d26f9721f4c6fc950f74bb8238af56fe616643
James <jamesaguilarf@gmail.com>
2024-02-15 11:38:24 +0100

    signup make

--- notes/src/components/SignUp.jsx
+++ notes/src/components/SignUp.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
-import './styles/home.css'
+
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles/signup.css";
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const makeFetch = (event) => {
+    fetch("http://localhost:8080/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 409) {
+            console.log("Este usuario ya existe");
+            return;
+          }
+          throw new Error("Signup ha fallado");
+        }
+        console.log("Sign up successful");
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <>
-      <p>Este es el registro</p>
+      <div className="card">
+        <div className="form">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              name="user"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button onClick={makeFetch}>Sign Up</button>
+        </div>
+      </div>
     </>
   );
 }
